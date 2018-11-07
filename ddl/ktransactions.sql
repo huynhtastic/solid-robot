f3f4d7e720de703e71db1331671292e9508a8d82
Richard Huynh <huynhtastic@gmail.com>
2018-11-07 01:06:19 -0600

    created report1 sql

--- ddl/ktransactions.sql
+++ ddl/ktransactions.sql
@@ -152,3 +152,19 @@ WHERE k.sender_id = e.emp_id
 ) a LEFT OUTER JOIN employees e
 ON a.recipient_id = e.emp_id
 ORDER BY txn_date ASC;
+
+SELECT Year, Month, username, gift, redeem,
+  RANK() OVER (PARTITION BY Year, Month ORDER BY gift DESC) AS rank
+FROM (
+  SELECT * FROM (
+  SELECT EXTRACT(YEAR FROM TXN_DATE) Year, EXTRACT(MONTH FROM TXN_DATE) Month,
+    username, nvl2(recipient_id, 'GIFT', 'REDEEM') Type, SUM(Amount) Amount
+  FROM ktransactions k, employees e
+  WHERE k.sender_id = e.emp_id
+  GROUP BY EXTRACT(YEAR FROM TXN_DATE), EXTRACT(MONTH FROM TXN_DATE), username, nvl2(recipient_id, 'GIFT', 'REDEEM')
+  ) PIVOT (
+    SUM(Amount)
+    FOR Type IN ('GIFT' AS Gift, 'REDEEM' AS Redeem)
+  )
+)
+ORDER BY Year DESC, Month DESC, rank ASC;
