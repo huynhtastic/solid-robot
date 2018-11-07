44f3846df0fe8dd33113a3e664467d9e713313e5
Richard Huynh <huynhtastic@gmail.com>
2018-11-07 00:17:25 -0600

    change messages to txn logs

--- db_apis/transactions.js
+++ db_apis/transactions.js
@@ -2,10 +2,19 @@ const oracledb = require('oracledb');
 const database = require('../services/database.js');
 
 const getTxnsQuery =
-  `SELECT e.username, k.amount, k.message
-   FROM employees e, ktransactions k
-   WHERE e.emp_id = k.sender_id AND k.recipient_id= :emp_id
-   ORDER BY txn_date ASC`;
+ ` SELECT txn_id, sender_id, sender, recipient_id,
+    e.username recipient,
+      amount, txn_date, message
+      FROM(
+      SELECT txn_id,
+    sender_id, e.username sender,
+    recipient_id,
+    amount, txn_date, message
+        FROM ktransactions k, employees e
+WHERE k.sender_id = e.emp_id AND (k.recipient_id= :emp_id OR k.sender_id = :emp_id)
+      ) a LEFT OUTER JOIN employees e
+ON a.recipient_id = e.emp_id
+ORDER BY txn_date ASC`;
 
 async function getTransactions(context) {
   let query = getTxnsQuery;
--- ddl/ktransactions.sql
+++ ddl/ktransactions.sql
@@ -138,3 +138,17 @@ BEGIN
         interval => 'SYSDATE + ((1/24) / 12)'
     );
 END;
+
+SELECT txn_id, sender_id, sender, recipient_id,
+  e.username recipient,
+  amount, txn_date, message
+FROM(
+SELECT txn_id,
+  sender_id, e.username sender,
+  recipient_id,
+  amount, txn_date, message
+FROM ktransactions k, employees e
+WHERE k.sender_id = e.emp_id
+) a LEFT OUTER JOIN employees e
+ON a.recipient_id = e.emp_id
+ORDER BY txn_date ASC;
